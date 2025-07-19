1282c042dc6046946aaa4c72d2699221651047f3
Saugat <pdl.saugat07@gmail.com>
2025-07-19 11:46:32 +0545

    Update: date format and css

--- frontend/notesApp/src/App.jsx
+++ frontend/notesApp/src/App.jsx
@@ -6,7 +6,7 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
   return (
-    <div data-theme="luxury">
+    <div className="bg-slate-900" data-theme="dim">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
